dec9ab937b44ef29634d9aa7a1509233b024faa2
Scrum_master.com <47978670+yusufade23@users.noreply.github.com>
2019-03-31 23:18:24 +0200

    Add files via upload

---
+++ CommunicatieViasockets/server.py
@@ -0,0 +1,21 @@
+import socket
+import time
+import pickle
+
+
+HEADERSIZE = 10
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1243))
+s.listen(5)
+
+while True:
+    # now our endpoint knows about the OTHER endpoint.
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established.")
+
+    d = {1:"Goedendag", 2: "Allemaal"}
+    msg = pickle.dumps(d)
+    msg = bytes(f"{len(msg):<{HEADERSIZE}}", 'utf-8')+msg
+    print(msg)
+    clientsocket.send(msg)
